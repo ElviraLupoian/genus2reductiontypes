289f3f50e6599c54f15b0a414b223c807286a927
aylwarde <aylwarde@tcd.ie>
2026-07-27 14:22:28 +0100

    added js and css files

---
+++ script.js
@@ -0,0 +1,40 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$']],
+      displayMath: [['$$', '$$']],
+  
+      macros: {
+        Z: '\\mathbb{Z}',
+        Q: '\\mathbb{Q}',
+        C: '\\mathbb{C}',
+  
+        D: '\\mathrm{D}',
+  
+        fs: '\\mathfrak{s}',
+        ft: '\\mathfrak{t}',
+  
+        Art: '\\operatorname{Art}',
+        Gal: '\\operatorname{Gal}',
+        Aut: '\\operatorname{Aut}',
+        GL: '\\operatorname{GL}',
+        PGL: '\\operatorname{PGL}',
+        disc: '\\operatorname{disc}',
+        Jac: '\\operatorname{Jac}',
+        codim: '\\operatorname{codim}',
+  
+        I: '\\mathrm{I}',
+        II: '\\mathrm{II}',
+        III: '\\mathrm{III}',
+        IV: '\\mathrm{IV}',
+        V: '\\mathrm{V}',
+        VI: '\\mathrm{VI}',
+        VII: '\\mathrm{VII}',
+  
+        cR: '\\mathcal{R}'
+      }
+    },
+  
+    svg: {
+      fontCache: 'global'
+    }
+  };
